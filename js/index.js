a75b91a10b266490aa2f473998beab282a23ff8f
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2022-10-27 18:52:47 -0700

    wee

--- js/index.js
+++ js/index.js
@@ -9,16 +9,39 @@ function cookie_prompt_handler(){
     }, 1000);   
 }
 
+function sleep(time, callback){
+    setInterval(callback, time);
+}
+
+function init_main() {
+    var header = document.getElementById("header");
+    var self_in = document.getElementById("self-intro");
+    header.classList.remove("opacity-0")
+
+    sleep(500, function() {
+
+    });
+}
+
 var app = angular.module("portfolio", []);
 window.addEventListener('load', function() {
-    document.getElementById("loading-logo").addEventListener("animationend", function() {
-        setInterval(() => {
-            document.getElementById("loading-screen").style.display = "none"; ;
-        }, 1000);
-        
-    })
+    var loading_screen = document.getElementById("loading-screen");
+    var loading_logo = document.getElementById("loading-logo");
 
-})
+    loading_logo.addEventListener("animationend", function(event) {
+        sleep(100, function () {
+            loading_screen.classList.add("opacity-0")
+            loading_screen.addEventListener("transitionend", function(event) {
+                loading_screen.classList.add("d-none"); 
+                init_main(); 
+            });
+        })
+
+    });
+});
+
+
+
 
 
 
